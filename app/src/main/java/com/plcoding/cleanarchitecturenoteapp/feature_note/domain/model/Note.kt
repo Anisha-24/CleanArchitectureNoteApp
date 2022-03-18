c03d4d2f13e1fca5a45ef38d19f85be747f0d9c4
Anisha-24 <anisha242002@gmail.com>
2022-03-19 00:20:50 +0530

    initial commit_anisha

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -3,6 +3,7 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model
 import android.graphics.Color
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.*
 
 @Entity
 data class Note(
@@ -11,6 +12,10 @@ data class Note(
     val timestamp : Long,
     val content : String,
     @PrimaryKey val id : Int? = null
-)
+) {
+    companion object{
+        val noteColors = listOf(RedOrange, BabyBlue, LightGreen, Violet, RedPink)
+    }
+}
 
 
