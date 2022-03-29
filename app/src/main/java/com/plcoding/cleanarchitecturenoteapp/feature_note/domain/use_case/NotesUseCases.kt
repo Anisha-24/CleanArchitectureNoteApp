809dafc44b046eb7f3661a74becf32acd77dcc78
Anisha-24 <anisha242002@gmail.com>
2022-03-29 23:45:21 +0530

    the whole draft

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
@@ -3,5 +3,6 @@ package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 data class NotesUseCases(
     val getNotesUseCase: GetNotesUseCase,
     val deleteNoteUseCase: DeleteNoteUseCase,
-    val addNoteUseCase: AddNoteUseCase
+    val addNoteUseCase: AddNoteUseCase,
+    val getNote: GetNote
 )
